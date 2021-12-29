de843933241df4effcb919775b948cb7aa2f1a5e
Alexander Betaev <a@betaev.pub>
2021-12-28 20:42:39 -0600

    repoop

--- core.ts
+++ core.ts
@@ -2,9 +2,9 @@ import type { } from './jsx.ts'
 
 export type ChildrenProps = { children: JSX.Children }
 
-type StatelessComponent<TProps extends JSX.Props = JSX.Props> = (props: TProps) => JSX.Element
-type StatefulComponent<TProps extends JSX.Props = JSX.Props> = (props: TProps) => AsyncGenerator<JSX.Element, JSX.Element | void, undefined>
-type SyntheticComponent<TProps extends JSX.Props = JSX.Props> = StatelessComponent<TProps> | StatefulComponent<TProps>
+type StaticComponent<TProps extends JSX.Props = JSX.Props> = (props: TProps) => JSX.Element
+type ActiveComponent<TProps extends JSX.Props = JSX.Props> = (props: TProps) => AsyncGenerator<JSX.Element, JSX.Element | void, undefined>
+type SyntheticComponent<TProps extends JSX.Props = JSX.Props> = StaticComponent<TProps> | ActiveComponent<TProps>
 
 type IntrinsicComponent = keyof JSX.IntrinsicElements
 
@@ -28,7 +28,7 @@ export const createElement = <TProps extends JSX.Props>(component: Component<TPr
 }
 
 type Context = Node[]
-export const render = (target: Element, context: Context, ...jsxElements: JSX.Element[]) => {
+export const render = (target: Element, children: JSX.Children, context: Context = []) => {
 
   const isTextElement = (element: JSX.Element): element is JSX.TextElement =>
     typeof element === "string" || typeof element === "boolean" || typeof element === "number"
@@ -36,10 +36,10 @@ export const render = (target: Element, context: Context, ...jsxElements: JSX.El
   const isDOMElement = (element: JSX.IntrinsicElement | JSX.SyntheticElement): element is JSX.IntrinsicElement =>
     "name" in element
 
-  const isStatefulElement = (element: JSX.SyntheticElement): element is JSX.SyntheticElement =>
+  const isActiveElement = (element: JSX.SyntheticElement): element is JSX.SyntheticElement =>
     "next" in element
 
-  jsxElements.forEach((jsxElement, index) => {
+  children && (Array.isArray(children) ? children : [children]).forEach((jsxElement, index) => {
     if (isTextElement(jsxElement)) {
       const node = document.createTextNode(`${jsxElement}`)
       const old = context[index]
@@ -54,23 +54,18 @@ export const render = (target: Element, context: Context, ...jsxElements: JSX.El
           .forEach(name => node.setAttribute(name, `${props[name]}`))
       if (children)
         (Array.isArray(children) ? children : [children])
-          .forEach(child => render(node, [], child))
+          .forEach(child => render(node, child, []))
       const old = context[index]
       context[index] = node
       if (old) target.replaceChild(node, old)
       else target.appendChild(node)
-    } else if (isStatefulElement(jsxElement)) {
+    } else if (isActiveElement(jsxElement)) {
       const context: Context = [];
       (async () => {
         for await (const element of { [Symbol.asyncIterator]: () => jsxElement }) {
-          render(target, context, element)
+          render(target, element, context)
         }
       })()
-    } else if (Array.isArray(jsxElement)) {
-      render(target, [], ...jsxElement as JSX.Element[])
-    } else {
-      console.log(`unsupported element`, jsxElement)
-    }
+    } else throw `unsupported element ${typeof jsxElement}`
   })
 }
-
